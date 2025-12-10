82e989b2f7b4426b80dc800876dbb8b366cd97a1
Dovydas Jarusevicius <dovydas.jarusevicius@bentley.com>
2025-12-10 16:31:03 +0200

    start of  part 2 day 10

--- 2025/day10.py
+++ 2025/day10.py
@@ -4,10 +4,12 @@ from itertools import combinations
 class Machine:
   indicatorDiagram: tuple[bool]
   buttons: tuple[tuple[int]]
+  joltage: tuple[int]
 
-  def __init__(self, indicatorDiagram: str, buttons: list[list[str]]):
+  def __init__(self, indicatorDiagram: str, buttons: list[list[str]], joltage: str):
     self.indicatorDiagram = tuple(True if indicator == "#" else False for indicator in indicatorDiagram)
     self.buttons = tuple(tuple(int(wire) for wire in button) for button in buttons)
+    self.joltage = tuple(int(jolt) for jolt in joltage.split(","))
 
 
 def pressButton(indicators: list[bool], button: tuple[int]) -> list[bool]:
@@ -17,7 +19,7 @@ def pressButton(indicators: list[bool], button: tuple[int]) -> list[bool]:
   return indicators
 
 
-def findFewestPresses(machine: Machine) -> int:
+def findFewestPressesIndicators(machine: Machine) -> int:
   buttonIds = list(range(0, len(machine.buttons)))
   presses = 1
   while presses <= len(machine.buttons):
@@ -34,12 +36,20 @@ def findFewestPresses(machine: Machine) -> int:
   return 0
 
 
+def findFewestPressesJoltages(machine: Machine) -> int:
+
+
 machines: list[Machine] = []
 for line in open("input10.txt").readlines():
   input = line.strip().split(" ")
-  machines.append(Machine(input[0][1:-1], [i[1:-1].split(",") for i in input[1:-1]]))
+  machines.append(Machine(input[0][1:-1], [i[1:-1].split(",") for i in input[1:-1]], input[-1][1:-1]))
 
 answer = 0
 for m in machines:
-  answer += findFewestPresses(m)
+  answer += findFewestPressesIndicators(m)
+print(answer)
+
+answer = 0
+for m in machines:
+  answer += findFewestPressesJoltages(m)
 print(answer)
